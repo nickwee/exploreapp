529272e8e17164cba1abbf3d78b2409178624a13
Nick Wee <nickwee@Nicks-MacBook-Pro.local>
2024-01-08 13:59:40 -0800

    added untracked files -- password mechanism working

---
+++ explore-app/src/components/ExploreApp.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export function ExploreApp() {
+  return (
+    <div>
+      {/* Main app functionality goes here */}
+      <h1>Welcome to the App!</h1>
+    </div>
+  );
+}
